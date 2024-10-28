31c5e3b703bcb4e8307c1daaf4c346029790e6c8
BuriKizilkaya <burak.kizilkaya@outlook.com>
2024-10-29 00:06:49 +0100

    update dotnet generic project

--- csharp/generic/src/Creatorly.SolutionName.Core.Tests/DemoTests.cs
+++ csharp/generic/src/Creatorly.SolutionName.Core.Tests/DemoTests.cs
@@ -1,23 +1,23 @@
-using Moq;
 using FluentAssertions;
+using NSubstitute;
 
 namespace Creatorly.SolutionName.Core.Tests;
 
 public class DemoTests
 {
     private Demo _sut;
-    private Mock<IDemoDownloader> _demoDownloaderMock;
+    private IDemoDownloader _demoDownloaderMock;
 
     public DemoTests()
     {
-        _demoDownloaderMock = new();
-        _sut = new("Test", _demoDownloaderMock.Object);
+        _demoDownloaderMock = NSubstitute.Substitute.For<IDemoDownloader>();
+        _sut = new("Test", _demoDownloaderMock);
     }
 
     [Fact]
     public async Task TestMethod1()
     {
-        _demoDownloaderMock.Setup(m => m.DownloadNameAsync()).ReturnsAsync("Test");
+        _demoDownloaderMock.DownloadNameAsync().Returns("Test");
         string result = await _sut.GetDownloadedNameAsync();
         result.Should().Be("Test");
     }
