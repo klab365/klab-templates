16e8f7751858a2de2f688f96e7f9d0868b24475e
BuriKizilkaya <burak.kizilkaya@outlook.com>
2025-03-24 15:01:42 +0100

    Update .net generic project

---
+++ csharp/generic/tests/Creatorly.SolutionName.Core.UnitTests/DemoTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using NSubstitute;
+
+namespace Creatorly.SolutionName.Core.UnitTests;
+
+public class DemoTests
+{
+    private readonly Demo _sut;
+    private readonly IDemoDownloader _demoDownloaderMock;
+
+    public DemoTests()
+    {
+        _demoDownloaderMock = Substitute.For<IDemoDownloader>();
+        _sut = new("Test", _demoDownloaderMock);
+    }
+
+    [Fact]
+    public async Task TestMethod1()
+    {
+        _demoDownloaderMock.DownloadNameAsync().Returns("Test");
+        string result = await _sut.GetDownloadedNameAsync();
+        result.Should().Be("Test");
+    }
+}
